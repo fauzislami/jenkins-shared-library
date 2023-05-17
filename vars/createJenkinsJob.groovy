53aa0f6297d720962ddb0afec008c78e060490f8
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-05-17 16:23:51 +0700

    Update createJenkinsJob.groovy

--- vars/createJenkinsJob.groovy
+++ vars/createJenkinsJob.groovy
@@ -1,32 +1,48 @@
 def call(String jobName, String credentialName, String stream, String jenkinsfilePath, Map<String, String> parameters) {
-    String jobScript = """
-pipeline {
-    agent any
-    parameters {
-        ${parameters.collect { paramName, paramValue -> "string(name: '${paramName}', defaultValue: '${paramValue}')\n" }.join()}
-    }
-    stages {
-        stage('Checkout') {
-            steps {
-                perforce credential: '${credentialName}', populate: autoClean(delete: true, modtime: false, parallel: [enable: false, minbytes: '1024', type: ''], pin: '', quiet: true, replace: true, tidy: false, viewMask: '', workspace: [charset: 'none', name: "jenkins-${NODE_NAME}-${JOB_NAME}-${EXECUTOR_NUMBER}", pinHost: false, spec: [[allwrite: false, clobber: false, compress: false, line: '', locked: false, modtime: false, rmdir: false, serverID: '', streamName: '${stream}', useTemplate: false, view: "\${WORKSPACE}/..."]]])
+    job(jobName) {
+        displayName(jobName)
+
+        // Parameters
+        parameters {
+            parameters.each { paramName, paramValue ->
+                stringParam(paramName, paramValue)
             }
         }
-        stage('Build') {
-            steps {
-                // Perform build steps here
+
+        // Perforce SCM configuration
+        scm {
+            perforce {
+                credential(credentialName)
+                depotPath(stream)
+                populate([
+                    stream: 'true',
+                    format: "jenkins-${NODE_NAME}-${JOB_NAME}-${EXECUTOR_NUMBER}",
+                    mapping: [
+                        view: "${stream}/..."
+                    ]
+                ])
+                workspaceView("${stream}/...")
             }
         }
-        // Add more stages as needed
-    }
-    post {
-        always {
-            cleanWs()
+
+        // Jenkinsfile configuration
+        definition {
+            cpsScm {
+                scm {
+                    perforce {
+                        credential(credentialName)
+                        populate([
+                            stream: 'true',
+                            format: "jenkins-${NODE_NAME}-${JOB_NAME}-${EXECUTOR_NUMBER}",
+                            mapping: [
+                                view: "${stream}/..."
+                            ]
+                        ])
+                        workspaceView("${stream}/...")
+                    }
+                }
+                scriptPath(jenkinsfilePath)
+            }
         }
     }
-}
-"""
-
-    writeFile file: 'dynamicJob/Jenkinsfile', text: jobScript
-
-    load 'dynamicJob'
 }
