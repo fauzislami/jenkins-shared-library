679a524d636cc1706fbd01f2ff4960ef023cc850
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-05-17 18:03:09 +0700

    Update createNewJob.groovy

--- vars/createNewJob.groovy
+++ vars/createNewJob.groovy
@@ -1,13 +1,42 @@
-// pipelineJob.groovy
-
-def call(String jobName, String dslScript) {
-    def jobDslScript = "pipelineJob('$jobName') {\n" +
-                       "  definition {\n" +
-                       "    cps {\n" +
-                       "      script('$dslScript')\n" +
-                       "    }\n" +
-                       "  }\n" +
-                       "}"
-
-    jobDsl(scriptText: jobDslScript)
+// createPipelineJob.groovy
+def call(String jobName, String repoUrl) {
+  pipelineJob(jobName) {
+    logRotator {
+      numToKeep(50)
+    }
+    definition {
+      cps {
+        script('''
+          pipeline {
+            agent any
+            libraries {
+              lib("pipeline-lib@master")
+            }
+            parameters {
+              gitParameter name: 'revision',
+                type: 'PT_BRANCH_TAG',
+                defaultValue: 'origin/master',
+                selectedValue: 'DEFAULT',
+                description: '',
+                branch: '',
+                branchFilter: '',
+                tagFilter: '',
+                useRepository: '',
+                quickFilterEnabled: true
+              }
+            stages {
+              stage('Build') {
+                steps {
+                  git branch: "${revision}",
+                    url: "${repoUrl}",
+                    credentialsId: 'github'
+                  myPipeline projectName: 'test-name'
+                }
+              }
+            }
+          }
+        ''')
+      }
+    }
+  }
 }
