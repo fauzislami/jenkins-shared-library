24ab62d9bdc450b995b0d612d4ff6a3dc620a6ee
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-05-17 17:07:54 +0700

    Update createNewProject.groovy

--- vars/createNewProject.groovy
+++ vars/createNewProject.groovy
@@ -1,15 +1,18 @@
 // createNewProject.groovy
 
-import javaposse.jobdsl.dsl.DslFactory
-
 def createNewProject(String projectName) {
-    def dslFactory = new DslFactory()
-    def jobDslScript = '''
-        job('${projectName}') {
-            // Configure your project here
-            // For example, SCM, build steps, etc.
-            // See Job DSL documentation for available options
+    jobDsl {
+        // Use the Job DSL syntax to define and configure the project
+        // For example, SCM, build steps, etc.
+        // See Job DSL documentation for available options
+        additionalClasspath('path/to/extra/jar1.jar')  // Optional: Add additional JARs if needed
+        additionalClasspath('path/to/extra/jar2.jar')
+        targets {
+            job(projectName) {
+                // Configure your project here
+                // For example, SCM, build steps, etc.
+                // See Job DSL documentation for available options
+            }
         }
-    '''
-    dslFactory.runScript(jobDslScript)
+    }
 }
