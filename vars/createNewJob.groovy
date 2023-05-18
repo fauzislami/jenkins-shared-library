698168c74cabe10cfdfa832ce4b150c7b300b791
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-05-18 11:23:05 +0700

    Update createNewJob.groovy

--- vars/createNewJob.groovy
+++ vars/createNewJob.groovy
@@ -1,24 +1,29 @@
 def call(String jobName, String paramName) {
-    pipelineJob(jobName) {
-        parameters {
-            stringParam(paramName, '', 'Name of the person')
-        }
-        definition {
-            cps {
-                script('''
-                    pipeline {
-                        agent any
-                        stages {
-                            stage('Greet') {
-                                steps {
-                                    echo "Hello!! \${params.${paramName}}"
+    jobDsl {
+        lightweight true
+        scriptText '''
+            pipelineJob("${jobName}") {
+                parameters {
+                    stringParam("${paramName}", '', 'Name of the person')
+                }
+                definition {
+                    cps {
+                        script('''
+                            pipeline {
+                                agent any
+                                stages {
+                                    stage('Greet') {
+                                        steps {
+                                            echo "Hello!! \${params.${paramName}}"
+                                        }
+                                    }
                                 }
                             }
-                        }
+                        '''.stripIndent())
+                        sandbox()
                     }
-                '''.stripIndent())
-                sandbox()
+                }
             }
-        }
+        '''
     }
 }
