7691b357f6f35082b2d718ffbcbe4b4f1ef1f4b0
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-05-18 10:18:46 +0700

    Update createNewJob.groovy

--- vars/createNewJob.groovy
+++ vars/createNewJob.groovy
@@ -1,17 +1,9 @@
-def call(String jobName) {
-    node {
-        multibranchPipelineJob(jobName) {
-            branchSources {
-                git {
-                    remote('https://github.com/your-repo.git')
-                    credentialsId('your-credentials-id')
-                    includes('*/master') // Update branch pattern as needed
-                }
-            }
-            configure { project ->
-                project / 'sources' / 'data' / 'jenkins.branch.BranchSource' / 'source' / 'git' / 'GitSCM' / 'excludedUsers' << 'your-excluded-user'
+def call(String jobName, String scriptContent) {
+    pipelineJob(jobName) {
+        definition {
+            cps {
+                script(scriptContent)
             }
         }
     }
-
 }
