3abb521d11a606cb264eb375d82f04c3150bd082
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-05-18 10:51:40 +0700

    Update createNewJob.groovy

--- vars/createNewJob.groovy
+++ vars/createNewJob.groovy
@@ -1,9 +1,42 @@
-def call(String jobName, String scriptContent) {
+def call(String jobName, String credentialId, String stream, String city, String province, String nation) {
     pipelineJob(jobName) {
         definition {
             cps {
-                script(scriptContent)
+                script('''
+                    pipeline {
+                        agent any
+                        
+                        parameters {
+                            string(name: 'city', defaultValue: '', description: 'City')
+                            string(name: 'province', defaultValue: '', description: 'Province')
+                            string(name: 'nation', defaultValue: '', description: 'Nation')
+                        }
+                        
+                        stages {
+                            stage('Build') {
+                                steps {
+                                    // Add your build steps here
+                                }
+                            }
+                            
+                            stage('Test') {
+                                steps {
+                                    // Add your test steps here
+                                }
+                            }
+                            
+                            stage('Deploy') {
+                                steps {
+                                    // Add your deployment steps here
+                                }
+                            }
+                        }
+                    }
+                ''')
             }
+        }
+        scm {
+            p4(credentialId, stream)
         }
     }
 }
