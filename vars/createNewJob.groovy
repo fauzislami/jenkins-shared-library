418c1c9e1f987aee46aed4ab7c8b2659eed3751b
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-05-18 10:38:27 +0700

    Update createNewJob.groovy

--- vars/createNewJob.groovy
+++ vars/createNewJob.groovy
@@ -1,21 +1,9 @@
-import hudson.model.*
-import jenkins.model.Jenkins
-
-def call(String jobName, Map<String, String> parameters) {
-    def triggerJob(String jobName, Map<String, String> parameters) {
-        def jenkins = Jenkins.instance
-        def job = jenkins.getItemByFullName(jobName)
-        def params = parameters.collect { paramName, paramValue ->
-            new StringParameterValue(paramName, paramValue)
+def call(String jobName, String scriptContent) {
+    pipelineJob(jobName) {
+        definition {
+            cps {
+                script(scriptContent)
+            }
         }
-        def paramsAction = new ParametersAction(params as Action[])
-
-        def queue = jenkins.getQueue()
-        def future = job.scheduleBuild2(0, paramsAction)
-        queue.schedule(future)
-
-        println "Job '${jobName}' triggered successfully with parameters: ${parameters}"
     }
-
-    triggerJob(jobName, parameters)
 }
