e5f526c76c3c4e0d407aafacdba262bfed4abc77
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-05-18 11:01:50 +0700

    Update createNewJob.groovy

--- vars/createNewJob.groovy
+++ vars/createNewJob.groovy
@@ -1,45 +1,24 @@
-def call(String jobName, String credentialId, String stream, String city, String province, String nation) {
-    jobDsl{
-        pipelineJob(jobName) {
-            definition {
-                cps {
-                    script('''
-                        pipeline {
-                            agent any
-                            
-                            parameters {
-                                string(name: 'city', defaultValue: '', description: 'City')
-                                string(name: 'province', defaultValue: '', description: 'Province')
-                                string(name: 'nation', defaultValue: '', description: 'Nation')
-                            }
-                            
-                            stages {
-                                stage('Build') {
-                                    steps {
-                                        // Add your build steps here
-                                    }
-                                }
-                                
-                                stage('Test') {
-                                    steps {
-                                        // Add your test steps here
-                                    }
-                                }
-                                
-                                stage('Deploy') {
-                                    steps {
-                                        // Add your deployment steps here
-                                    }
+def call(String jobName, String paramName) {
+    pipelineJob(jobName) {
+        parameters {
+            stringParam(paramName, '', 'Name of the person')
+        }
+        definition {
+            cps {
+                script('''
+                    pipeline {
+                        agent any
+                        stages {
+                            stage('Greet') {
+                                steps {
+                                    echo "Hello!! \${params.${paramName}}"
                                 }
                             }
                         }
-                    ''')
-                }
-            }
-            scm {
-                p4(credentialId, stream)
+                    }
+                '''.stripIndent())
+                sandbox()
             }
         }
     }
-
 }
