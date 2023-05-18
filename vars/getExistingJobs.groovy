72cf496f2b5eb672388861516ca297c0b00f5e5c
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-05-18 19:05:44 +0700

    Create checkExistingJobs.groovy

---
+++ vars/getExistingJobs.groovy
@@ -0,0 +1,14 @@
+import jenkins.model.Jenkins
+
+def call(Map parameters) {
+  def jobsToTrigger = parameters.jobsToTrigger
+  def jenkins = Jenkins.instance
+  def existingJobs = jenkins.getItems()
+
+  for (jobToTrigger in jobsToTrigger) {
+      def jobExists = existingJobs.find { job -> job.name == jobToTrigger.job }
+      if (!jobExists) {
+          println "Creating '${jobToTrigger.job}'."
+      }  
+  }
+}
