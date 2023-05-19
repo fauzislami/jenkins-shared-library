8af67636d87a98a2c60ce0b2665ebed666880c8b
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-05-19 16:49:49 +0700

    Update getExistingJobs.groovy

--- vars/getExistingJobs.groovy
+++ vars/getExistingJobs.groovy
@@ -14,31 +14,3 @@ def call(Map parameters) {
 }
 
 
-
-// import jenkins.model.Jenkins
-
-// def listJobs() {
-//     def filePath = new File('/var/jenkins_home/workspace/Master Pipeline/listOfJobs.groovy').text
-//     def fileContent = filePath
-// }
-
-// def readListJobs(Map parameters) {
-//     def jobs = listJobs()
-//     return jobs
-// }
-
-// def call() {
-//     def jobs = readListJobs()
-//     def jenkins = Jenkins.instance
-//     def existingJobs = jenkins.getItems()
-
-//     for (jobToTrigger in jobs) {
-//         def jobExists = existingJobs.find { job -> job.name == jobToTrigger.job }
-//         if (!jobExists) {
-//             createNewJob([existingJobName: 'test-1', newJobName: jobToTrigger.job ]) // Change 'test-1' with the template we created before
-//         }
-//     }
-// }
-
-
-
