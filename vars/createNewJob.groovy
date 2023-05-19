7c1173f02c97703cebd5d017231bcca4fca4bab5
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-05-19 18:20:49 +0700

    Update createNewJob.groovy

--- vars/createNewJob.groovy
+++ vars/createNewJob.groovy
@@ -42,6 +42,36 @@
 //         println "Existing Job '${existingJobName}' not found."
 //     }
 // }
+
+// import jenkins.model.Jenkins
+
+// def call(Map parameters) {
+//     def existingJobName = parameters.existingJobName
+//     def newJobName = parameters.newJobName
+//     def jenkins = Jenkins.instance
+//     def existingJob = jenkins.getItemByFullName(existingJobName)
+
+//     if (existingJob) {
+//         def targetFolder = jenkins.getItemByFullName(newJobName.substring(0, newJobName.lastIndexOf("/")))
+
+//         if (targetFolder && targetFolder instanceof com.cloudbees.hudson.plugins.folder.Folder) {
+//             def newJob = targetFolder.getItem(newJobName.substring(newJobName.lastIndexOf("/") + 1))
+
+//             if (newJob) {
+//                 println "Job '${newJobName}' already exists."
+//             } else {
+//                 newJob = targetFolder.copy(existingJob, newJobName.substring(newJobName.lastIndexOf("/") + 1))
+//                 newJob.save()
+//                 println "New Job '${newJobName}' created successfully in '${targetFolder.fullName}'!"
+//             }
+//         } else {
+//             println "Target Folder '${newJobName.substring(0, newJobName.lastIndexOf("/"))}' not found or is not a valid folder."
+//         }
+//     } else {
+//         println "Existing Job '${existingJobName}' not found."
+//     }
+// }
+
 import jenkins.model.Jenkins
 
 def call(Map parameters) {
@@ -55,14 +85,12 @@ def call(Map parameters) {
 
         if (targetFolder && targetFolder instanceof com.cloudbees.hudson.plugins.folder.Folder) {
             def newJob = targetFolder.getItem(newJobName.substring(newJobName.lastIndexOf("/") + 1))
-
-            if (newJob) {
+            if (!newJob) {
                 println "Job '${newJobName}' already exists."
-            } else {
                 newJob = targetFolder.copy(existingJob, newJobName.substring(newJobName.lastIndexOf("/") + 1))
                 newJob.save()
                 println "New Job '${newJobName}' created successfully in '${targetFolder.fullName}'!"
-            }
+            }     
         } else {
             println "Target Folder '${newJobName.substring(0, newJobName.lastIndexOf("/"))}' not found or is not a valid folder."
         }
