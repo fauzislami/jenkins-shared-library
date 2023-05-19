0a8e26185eff938dc441551a5abd922a2dc6ff98
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-05-19 10:47:44 +0700

    Update getExistingJobs.groovy

--- vars/getExistingJobs.groovy
+++ vars/getExistingJobs.groovy
@@ -17,33 +17,34 @@
 
 import jenkins.model.Jenkins
 
-def call(String fileName) {
-    def readAndPrintListOfJobs() {
-        def fileContent = new File(fileName).text
-        def script = evaluate(fileContent)
-        def jobsToTrigger = script.jobsToTrigger
+def readAndPrintListOfJobs(String fileName) {
+    def fileContent = new File(fileName).text
+    def script = evaluate(fileContent)
+    def jobsToTrigger = script.jobsToTrigger
 
-        jobsToTrigger.each { job ->
-            println("Job: ${job.job}")
-            job.params.each { param ->
-                println("Parameter - Name: ${param.name}, Value: ${param.value}")
-            }
-            println()
+    jobsToTrigger.each { job ->
+        println("Job: ${job.job}")
+        job.params.each { param ->
+            println("Parameter - Name: ${param.name}, Value: ${param.value}")
+        }
+        println()
+    }
+
+    return jobsToTrigger
+}
+
+def call(String fileName) {
+    def jobsToTrigger = readAndPrintListOfJobs(fileName)
+    def jenkins = Jenkins.instance
+    def existingJobs = jenkins.getItems()
+
+    for (jobToTrigger in jobsToTrigger) {
+        def jobExists = existingJobs.find { job -> job.name == jobToTrigger.job }
+        if (!jobExists) {
+            createNewJob([existingJobName: 'test-1', newJobName: jobToTrigger.job ]) // Change 'test-1' with the template we created before
         }
     }
-
-    def call() {
-      def jobsToTrigger = readAndPrintListOfJobs()
-      def jenkins = Jenkins.instance
-      def existingJobs = jenkins.getItems()
-
-      for (jobToTrigger in jobsToTrigger) {
-          def jobExists = existingJobs.find { job -> job.name == jobToTrigger.job }
-          if (!jobExists) {
-              createNewJob([existingJobName: 'test-1', newJobName: jobToTrigger.job ]) //change 'test-1' with the template we created before
-          }  
-      }
-    }
 }
+
 
 
