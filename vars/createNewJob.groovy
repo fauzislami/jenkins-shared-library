dcf56864f3311deb1c4d33bab6300e0ca024fd01
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-05-19 18:08:43 +0700

    Update createNewJob.groovy

--- vars/createNewJob.groovy
+++ vars/createNewJob.groovy
@@ -1,26 +1,3 @@
-// import jenkins.model.Jenkins
-
-// def call(Map parameters) {
-//     def existingJobName = parameters.existingJobName
-//     def newJobName = parameters.newJobName
-    
-//     // Get the Jenkins instance
-//     def jenkins = Jenkins.instance
-
-//     // Get the existing job
-//     def existingJob = jenkins.getItem(existingJobName)
-
-//     if (existingJob) {
-//         def newJob = jenkins.copy(existingJob, newJobName)
-//         newJob.save()
-
-//         println "New Job '${newJobName}' created successfully!"
-//     } else {
-//         println "Existing Job '${existingJobName}' not found."
-//     }
-// }
-
-
 import jenkins.model.Jenkins
 
 def call(Map parameters) {
