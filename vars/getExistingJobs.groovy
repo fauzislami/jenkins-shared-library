2aa85c165ce74d9874c75fb919b51cd732eb1b91
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-05-19 14:09:00 +0700

    Update getExistingJobs.groovy

--- vars/getExistingJobs.groovy
+++ vars/getExistingJobs.groovy
@@ -1,44 +1,44 @@
-// import jenkins.model.Jenkins
-
-// def call(Map parameters) {
-//   def jobsToTrigger = parameters.jobsToTrigger
-//   def jenkins = Jenkins.instance
-//   def existingJobs = jenkins.getItems()
-
-//   for (jobToTrigger in jobsToTrigger) {
-//       def jobExists = existingJobs.find { job -> job.name == jobToTrigger.job }
-//       if (!jobExists) {
-//           createNewJob([existingJobName: 'test-1', newJobName: jobToTrigger.job ]) //change 'test-1' with the template we created before
-//       }  
-//   }
-// }
-
-
-
 import jenkins.model.Jenkins
 
-def listJobs() {
-    def filePath = new File('/var/jenkins_home/workspace/Master Pipeline/listOfJobs.groovy').text
-    def fileContent = filePath
+def call(Map parameters) {
+  def jobsToTrigger = parameters.jobsToTrigger
+  def jenkins = Jenkins.instance
+  def existingJobs = jenkins.getItems()
+
+  for (jobToTrigger in jobsToTrigger) {
+      def jobExists = existingJobs.find { job -> job.name == jobToTrigger.job }
+      if (!jobExists) {
+          createNewJob([existingJobName: 'test-1', newJobName: jobToTrigger.job ]) //change 'test-1' with the template we created before
+      }  
+  }
 }
 
-def readListJobs(Map parameters) {
-    def jobs = listJobs()
-    return jobs
-}
 
-def call() {
-    def jobs = readListJobs()
-    def jenkins = Jenkins.instance
-    def existingJobs = jenkins.getItems()
 
-    for (jobToTrigger in jobs) {
-        def jobExists = existingJobs.find { job -> job.name == jobToTrigger.job }
-        if (!jobExists) {
-            createNewJob([existingJobName: 'test-1', newJobName: jobToTrigger.job ]) // Change 'test-1' with the template we created before
-        }
-    }
-}
+// import jenkins.model.Jenkins
+
+// def listJobs() {
+//     def filePath = new File('/var/jenkins_home/workspace/Master Pipeline/listOfJobs.groovy').text
+//     def fileContent = filePath
+// }
+
+// def readListJobs(Map parameters) {
+//     def jobs = listJobs()
+//     return jobs
+// }
+
+// def call() {
+//     def jobs = readListJobs()
+//     def jenkins = Jenkins.instance
+//     def existingJobs = jenkins.getItems()
+
+//     for (jobToTrigger in jobs) {
+//         def jobExists = existingJobs.find { job -> job.name == jobToTrigger.job }
+//         if (!jobExists) {
+//             createNewJob([existingJobName: 'test-1', newJobName: jobToTrigger.job ]) // Change 'test-1' with the template we created before
+//         }
+//     }
+// }
 
 
 
