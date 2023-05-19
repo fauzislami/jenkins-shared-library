ca1702fabe7a4e8c1663de2eb491b0eb5ae37ed5
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-05-19 17:12:50 +0700

    Update getExistingJobs.groovy

--- vars/getExistingJobs.groovy
+++ vars/getExistingJobs.groovy
@@ -1,6 +1,6 @@
 import jenkins.model.Jenkins
 
-def call(Map parameters) {
+def call(Map parameters, String template) {
   def jobsToTrigger = parameters.jobsToTrigger
   def jenkins = Jenkins.instance
   def existingJobs = jenkins.getItems()
@@ -8,7 +8,8 @@ def call(Map parameters) {
   for (jobToTrigger in jobsToTrigger) {
       def jobExists = existingJobs.find { job -> job.name == jobToTrigger.job }
       if (!jobExists) {
-          createNewJob([existingJobName: 'test-1', newJobName: jobToTrigger.job ]) //change 'test-1' with the template we created before
+//           createNewJob([existingJobName: 'test-1', newJobName: jobToTrigger.job ]) //change 'test-1' with the template we created before
+             createNewJob([existingJobName: template, newJobName: jobToTrigger.job ])
       }  
   }
 }
