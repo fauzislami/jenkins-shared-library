cc49cab49b8044bbd57039d44a946a51d06c31f3
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-06-27 22:24:35 +0700

    Update getExistingJobs.groovy

--- vars/getExistingJobs.groovy
+++ vars/getExistingJobs.groovy
@@ -1,19 +1,3 @@
-// import jenkins.model.Jenkins
-
-// def call(Map parameters, String template) {
-//   def jobsToTrigger = parameters.jobsToTrigger
-//   def jenkins = Jenkins.instance
-//   def existingJobs = jenkins.getItems()
-
-//   for (jobToTrigger in jobsToTrigger) {
-//       def jobExists = existingJobs.find { job -> job.name == jobToTrigger.job }
-//       if (!jobExists) {
-//           createNewJob([existingJobName: template, newJobName: jobToTrigger.job ])
-//       }  
-//   }
-// }
-
-
 import jenkins.model.Jenkins
 
 def call(Map parameters) {
