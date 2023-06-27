75618a25d779e023b52db5ff8d12a504fa586eda
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-06-27 22:36:31 +0700

    Update getExistingJobs.groovy

--- vars/getExistingJobs.groovy
+++ vars/getExistingJobs.groovy
@@ -1,15 +1,35 @@
+// import jenkins.model.Jenkins
+
+// def call(Map parameters) {
+//   def jobsToTrigger = parameters.jobsToTrigger
+//   def jobTemplate = parameters.jobTemplate
+//   def jenkins = Jenkins.instance
+//   def existingJobs = jenkins.getItems()
+
+//   for (jobToTrigger in jobsToTrigger) {
+//       def jobExists = existingJobs.find { job -> job.name == jobToTrigger.job }
+//       if (!jobExists) {
+//           createNewJob([existingJobName: jobTemplate, newJobName: jobToTrigger.job ])
+//       }  
+//   }
+// }
+
+
+
 import jenkins.model.Jenkins
 
 def call(Map parameters) {
-  def jobsToTrigger = parameters.jobsToTrigger
-  def jobTemplate = parameters.jobTemplate
-  def jenkins = Jenkins.instance
-  def existingJobs = jenkins.getItems()
+    def jenkins = Jenkins.instance
+    def existingJobs = jenkins.getItems()
 
-  for (jobToTrigger in jobsToTrigger) {
-      def jobExists = existingJobs.find { job -> job.name == jobToTrigger.job }
-      if (!jobExists) {
-          createNewJob([existingJobName: jobTemplate, newJobName: jobToTrigger.job ])
-      }  
-  }
+    parameters.each { key, value ->
+        value.each { jobToTrigger ->
+            def jobExists = existingJobs.find { job -> job.name == jobToTrigger.job }
+            if (!jobExists) {
+                createNewJob([existingJobName: parameters.jobTemplate, newJobName: jobToTrigger.job])
+            }
+        }
+    }
 }
+
+return this
