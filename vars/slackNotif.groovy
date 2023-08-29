c238904fd260495da9a35ccdb25506cf8ea3fe4f
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-08-29 20:36:38 +0700

    Update slackNotif.groovy

--- vars/slackNotif.groovy
+++ vars/slackNotif.groovy
@@ -1,47 +1,45 @@
 def call(List<String> groovyFiles) {
-node {
-        def jobResultsByType = [:]
-        def combinedMessage = ""
-    
-        for (groovyFile in groovyFiles) {
-            def jobType = groovyFile.tokenize('.')[0]
-            def varsFile = load groovyFile
-            def allJobs = BaseJobs + PlatformsJobs
-            
-            
-            for (job in allJobs) {
-                def jobName = job.job
-                def build = retrieveLatestBuild(jobName)
-                def buildResult = "${build.result}"
-                def buildUrl = build.getAbsoluteUrl()
-    
-                if (buildResult != "SUCCESS") {
-                    def emoji = buildResult == "FAILURE" ? ":x:" : ":no_entry_sign:"
-                    if (!jobResultsByType.containsKey(jobType)) {
-                        jobResultsByType[jobType] = []
-                    }
-                    jobResultsByType[jobType].add("[${jobName}] - <${buildUrl}|See here> - ${buildResult} $emoji")
+    def jobResultsByType = [:]
+    def combinedMessage = ""
+    def PlatformsJobs = []
+    def BaseJobs = []
+
+    for (groovyFile in groovyFiles) {
+        def jobType = groovyFile.tokenize('.')[0]
+        def varsFile = load groovyFile
+        def allJobs = BaseJobs + PlatformsJobs
+
+        for (job in allJobs) {
+            def jobName = job.job
+            def build = retrieveLatestBuild(jobName)
+            def buildResult = "${build.result}"
+            def buildUrl = build.getAbsoluteUrl()
+
+            if (buildResult != "SUCCESS") {
+                def emoji = buildResult == "FAILURE" ? ":x:" : ":no_entry_sign:"
+                if (!jobResultsByType.containsKey(type)) {
+                    jobResultsByType[jobType] = []
                 }
+                jobResultsByType[jobType].add("[${jobName}] - <${buildUrl}|See here> - ${buildResult} $emoji")
             }
         }
-    
-        jobResultsByType.each { version, results ->
-            if (!results.isEmpty()) {
-                combinedMessage += "*THE FOLLOWING JOBS FAILED FOR ${version}:*\n" + results.join('\n') + "\n"
-            }
+    }
+
+    jobResultsByType.each { version, results ->
+        if (!results.isEmpty()) {
+            combinedMessage += "*THE FOLLOWING JOBS FAILED FOR ${version}:*\n" + results.join('\n') + "\n"
         }
-    
-        if (!combinedMessage.isEmpty()) {
-            slackSend(channel: '#jenkins-notif-test', message: combinedMessage, color: 'danger')
-        } 
-        else {
-           slackSend(channel: '#jenkins-notif-test', message: "All jobs succeed :white_check_mark:", color: 'good')
-        }
+    }
+
+    if (!combinedMessage.isEmpty()) {
+        slackSend(channel: '#jenkins-notif-test', message: combinedMessage, color: 'danger')
+    } else {
+        slackSend(channel: '#jenkins-notif-test', message: "All jobs succeed :white_check_mark:", color: 'good')
     }
 }
 
+// Define the retrieveLatestBuild function here
 def retrieveLatestBuild(jobName) {
     def build = jenkins.model.Jenkins.instance.getItemByFullName(jobName).getLastBuild()
     return build
 }
-
