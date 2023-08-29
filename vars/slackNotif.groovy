43c4fad3fe7016f0f9bd6c760467c4b4e443d069
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-08-29 23:03:09 +0700

    Update slackNotif.groovy

--- vars/slackNotif.groovy
+++ vars/slackNotif.groovy
@@ -4,7 +4,7 @@ def call(List<String> groovyFiles) {
 
     for (groovyFile in groovyFiles) {
         def jobType = groovyFile.tokenize('.')[0]
-        //def varsFile = load groovyFile
+        def varsFile = load groovyFile
         def allJobs = BaseJobs + PlatformsJobs
 
         for (job in allJobs) {
