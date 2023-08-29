a2d1f77be3c2c847cde8e447e8807261512079a3
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-08-29 15:11:55 +0700

    Update slackNotif.groovy

--- vars/slackNotif.groovy
+++ vars/slackNotif.groovy
@@ -5,7 +5,34 @@ def call(List<String> groovyFiles) {
     for (groovyFile in groovyFiles) {
         def jobType = groovyFile.tokenize('.')[0]
         def varsFile = load groovyFile
-        println varsFile
+        //def allJobs = BaseJobs + PlatformsJobs
+
+        for (job in PlatformsJobs) {
+            def jobName = job.job
+            def build = retrieveLatestBuild(jobName)
+            def buildResult = "${build.result}"
+            def buildUrl = build.getAbsoluteUrl()
+
+            if (buildResult != "SUCCESS") {
+                def emoji = buildResult == "FAILURE" ? ":x:" : ":no_entry_sign:"
+                if (!jobResultsByType.containsKey(type)) {
+                    jobResultsByType[jobType] = []
+                }
+                jobResultsByType[jobType].add("[${jobName}] - <${buildUrl}|See here> - ${buildResult} $emoji")
+            }
+        }
+    }
+
+    jobResultsByType.each { version, results ->
+        if (!results.isEmpty()) {
+            combinedMessage += "*THE FOLLOWING JOBS FAILED FOR ${version}:*\n" + results.join('\n') + "\n"
+        }
+    }
+
+    if (!combinedMessage.isEmpty()) {
+        slackSend(channel: '#jenkins-notif-test', message: combinedMessage, color: 'danger')
+    } else {
+        slackSend(channel: '#jenkins-notif-test', message: "All jobs succeed :white_check_mark:", color: 'good')
     }
 }
 
