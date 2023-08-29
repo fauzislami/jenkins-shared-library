4355b070c6a43222df0f09075867c8c1da70377a
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-08-29 15:02:22 +0700

    Update slackNotif.groovy

--- vars/slackNotif.groovy
+++ vars/slackNotif.groovy
@@ -1,6 +1,11 @@
 def call(List<String> groovyFiles) {
     def jobResultsByType = [:]
     def combinedMessage = ""
+
+    for (groovyFile in groovyFiles) {
+        def jobType = groovyFile.tokenize('.')[0]
+        def varsFile = load groovyFile
+    }
 }
 
 // Define the retrieveLatestBuild function here
