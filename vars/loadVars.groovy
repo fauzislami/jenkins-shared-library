fc00fb124b5d524b3b79b464b6083642749fdc3f
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-08-29 23:48:35 +0700

    Update loadVars.groovy

--- vars/loadVars.groovy
+++ vars/loadVars.groovy
@@ -1,9 +1,7 @@
-//def call(List<String> groovyFiles) {
-def call() {
-  //for (groovyFile in groovyFiles) {
-      //def ueVersion = groovyFile.tokenize('.')[0]
-      //def varsFile = load groovyFile
-      //def allJobs = BaseJobs + PlatformsJobs
-      println BaseJobs
-  //}
+def call(List<String> groovyFiles) {
+  for (groovyFile in groovyFiles) {
+      def ueVersion = groovyFile.tokenize('.')[0]
+      def varsFile = load groovyFile
+      def allJobs = BaseJobs + PlatformsJobs
+  }
 }
